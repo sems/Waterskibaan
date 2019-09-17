a7bf73695c81c2bcb71aca3958a6fd736f8dc910
Sem Spanhaak <semspanhaak@gmail.com>
2019-09-17 12:14:15 +0200

    Opdracht 2

---
+++ Waterskibaan/Kabel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Kabel
+    {
+        private LinkedList<Lijn> _lijnen;
+
+        public Boolean IsStartPositieLeeg()
+        {
+            return true;
+        }
+
+        public void NeemLijnInGebruik(Lijn lijn)
+        {
+
+        }
+
+        public void VerschuifLijnen()
+        {
+            
+        }
+        public Lijn VerwijderLijnVanKabel()
+        {
+            return new Lijn();
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Waterskibaan/Sporter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Sporter
+    {
+        public int AantalRondenNogTeGaan { get; set; }
+        public Zwemvest Zwemvest { get; set; }
+        public Skies Skies { get; set; }
+        public Color KledingKleur { get; set; }
+        public List<IMoves> Moves { get; set; }
+
+        public Sporter(List<IMoves> moves)
+        {
+            AantalRondenNogTeGaan = 0;
+            Moves = moves;
+        }
+    }
+}
