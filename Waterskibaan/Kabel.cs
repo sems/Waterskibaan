c2b203b438bb23e6063cfc37a0a8d735e458994d
Sem Spanhaak <semspanhaak@gmail.com>
2019-09-23 15:35:41 +0200

    Opgave 2

--- Waterskibaan/Kabel.cs
+++ Waterskibaan/Kabel.cs
@@ -10,27 +10,92 @@ namespace Waterskibaan
     {
         private LinkedList<Lijn> _lijnen;
 
-        public Boolean IsStartPositieLeeg()
+        public Kabel()
         {
+            _lijnen = new LinkedList<Lijn>();
+        }
+
+        public bool IsStartPositieLeeg()
+        {
+            var node = _lijnen.First;
+            if (node == null)
+            {
+                return true;
+            }
+            while (node != null)
+            {
+                var nextNode = node.Next;
+                if (node.Value.PostitieOpdeKabel == 0)
+                {
+                    return false;
+                }
+                node = nextNode;
+            }
             return true;
+
+            // Old code
+            //if (_lijnen.First.Value.PostitieOpdeKabel != 0 || _lijnen.First == null)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
         }
 
         public void NeemLijnInGebruik(Lijn lijn)
         {
-
+            if (IsStartPositieLeeg())
+            {
+                _lijnen.AddFirst(lijn);
+            }
         }
 
         public void VerschuifLijnen()
         {
-            
+            foreach (Lijn item in _lijnen)
+            {
+                item.PostitieOpdeKabel += 1;
+                if (item.PostitieOpdeKabel > 9)
+                {
+                    item.PostitieOpdeKabel = 0;
+                }
+            }
         }
         public Lijn VerwijderLijnVanKabel()
         {
-            return new Lijn();
+            var node = _lijnen.First;
+            while (node != null)
+            {
+                var nextNode = node.Next;
+                if (node.Value.PostitieOpdeKabel == 9)
+                {
+                    return node.Value;
+                }
+                node = nextNode;
+            }
+            return null;
+        }
+        public bool isEmpty()
+        {
+            return _lijnen == null;
         }
         public override string ToString()
         {
-            return base.ToString();
+            if (isEmpty())
+            {
+                return "";
+            }
+            else
+            {
+                var node = _lijnen.First;
+                while (node != null)
+                {
+                    return $"{ node.Value.PostitieOpdeKabel}|";
+                }
+            }
+            return "";
         }
     }
 }
--- Waterskibaan/Program.cs
+++ Waterskibaan/Program.cs
@@ -10,6 +10,27 @@ namespace Waterskibaan
     {
         static void Main(string[] args)
         {
+            Program.TestOpdracht2();
+        }
+
+        private static void TestOpdracht2()
+        {
+            Kabel cable = new Kabel();
+
+            Lijn l1 = new Lijn();
+            l1.PostitieOpdeKabel = 0;
+            Lijn l2 = new Lijn();
+            l1.PostitieOpdeKabel = 5;
+            Lijn l3 = new Lijn();
+            l1.PostitieOpdeKabel = 6;
+
+            cable.NeemLijnInGebruik(l1);
+            cable.NeemLijnInGebruik(l2);
+            cable.NeemLijnInGebruik(l3);
+
+            cable.VerschuifLijnen();
+
+            Console.WriteLine(cable);
         }
     }
 }
