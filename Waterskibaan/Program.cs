f453fcde54a22203f9818ab4412eedd50ff87875
Sem Spanhaak <semspanhaak@gmail.com>
2019-10-03 20:42:25 +0200

    Added TestOpdracht10

--- Waterskibaan/Program.cs
+++ Waterskibaan/Program.cs
@@ -10,13 +10,7 @@ namespace Waterskibaan
     {
         static void Main(string[] args)
         {
-            //Program.TestOpdracht2();
-            //Program.TestOpdracht3();
-            //Program.TestOpdracht8();
-
-            InstructieGroep i = new InstructieGroep();
-            //i.SportersVerlatenRij(3);
-            Console.WriteLine(i.Rij);
+            TestOpdracht10();
         }
 
         private static void TestOpdracht2()
@@ -67,6 +61,62 @@ namespace Waterskibaan
             //s.Skies = new Skies();
 
             w.SporterStart(s);
+        }
+
+        private static void TestOpdracht10()
+        {
+            Sporter s = new Sporter(Movecollection.GetWillekeurigeMoves());
+            s.Zwemvest = new Zwemvest();
+            s.Skies = new Skies();
+
+            Sporter s1 = new Sporter(Movecollection.GetWillekeurigeMoves());
+            s1.Zwemvest = new Zwemvest();
+            s1.Skies = new Skies();
+
+            InstructieGroep ig = new InstructieGroep();
+            ig.SporterNeemPlaatsInRij(s);
+            ig.SporterNeemPlaatsInRij(s1);
+
+            Console.WriteLine(ig);
+
+            ig.SportersVerlatenRij(1);
+
+            Console.WriteLine(ig);
+
+            Sporter s2 = new Sporter(Movecollection.GetWillekeurigeMoves());
+            s2.Zwemvest = new Zwemvest();
+            s2.Skies = new Skies();
+
+            Sporter s3 = new Sporter(Movecollection.GetWillekeurigeMoves());
+            s3.Zwemvest = new Zwemvest();
+            s3.Skies = new Skies();
+
+            Sporter s4 = new Sporter(Movecollection.GetWillekeurigeMoves());
+            s4.Zwemvest = new Zwemvest();
+            s4.Skies = new Skies();
+
+            Sporter s5 = new Sporter(Movecollection.GetWillekeurigeMoves());
+            s5.Zwemvest = new Zwemvest();
+            s1.Skies = new Skies();
+
+            Sporter s6 = new Sporter(Movecollection.GetWillekeurigeMoves());
+            s6.Zwemvest = new Zwemvest();
+            s6.Skies = new Skies();
+
+            ig.SporterNeemPlaatsInRij(s);
+            ig.SporterNeemPlaatsInRij(s2);
+            ig.SporterNeemPlaatsInRij(s3);
+            ig.SporterNeemPlaatsInRij(s4);
+            
+            // Deze passen niet
+            ig.SporterNeemPlaatsInRij(s5);
+            ig.SporterNeemPlaatsInRij(s6);
+
+            var sporters = ig.GetAlleSporters();
+            foreach (var sporter in sporters)
+            {
+                Console.WriteLine(sporter);
+            }
         }
     }
 }
